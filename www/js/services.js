f3dc11cb34f6e6515272603cbc1e8a5d3f26e40a
Sean Reece <reece.sean@gmail.com>
2015-03-01 00:15:55 -0500

    Fix time display

--- www/js/services.js
+++ www/js/services.js
@@ -204,23 +204,23 @@ angular.module('shoutie.services', ['ngResource'])
 
                 var interval = Math.floor(seconds / 31536000);
 
-                if (interval > 1) {
+                if (interval >= 1) {
                     return interval + " years ago";
                 }
                 interval = Math.floor(seconds / 2592000);
-                if (interval > 1) {
+                if (interval >= 1) {
                     return interval + " months ago";
                 }
                 interval = Math.floor(seconds / 86400);
-                if (interval > 1) {
+                if (interval >= 1) {
                     return interval + " days ago";
                 }
                 interval = Math.floor(seconds / 3600);
-                if (interval > 1) {
+                if (interval >= 1) {
                     return interval + " hrs ago";
                 }
                 interval = Math.floor(seconds / 60);
-                if (interval > 1) {
+                if (interval >= 1) {
                     return interval + " min ago";
                 }
                 return Math.floor(seconds) + " sec ago";
